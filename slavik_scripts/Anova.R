32205d50f02cc742e35fa016601d0c68ddc9bd21
Dodan <mihai.dodan@gmail.com>
2015-05-28 01:02:02 +0300

    Update Anova.R

--- slavik_scripts/Anova.R
+++ slavik_scripts/Anova.R
@@ -1,25 +1,29 @@
-dataSetup<-function(){
+prepareAnova<-function(){
+  #Ecommerce
   wattsupcommerce<-read.csv("wr_ecommerce_load.TXT", sep="", header=T, row.names=NULL, stringsAsFactors=FALSE)
-  
-  #convert the timestamp in suitable format
   Timestamp<-as.POSIXct(as.numeric(wattsupcommerce[,1])*24*3600, origin="1899-12-30 00:00:00.000", tz="UTC")
+  wattsupcommerce <- data.frame(wattsupcommerce[2])
+  wattsupcommerce["Testtype"]<- NA
+  wattsupcommerce$Testtype[1:121]<-"Ecommerce" 
   
-  #subset the energy data, add the timestamp as first column
-  wattsupcommerce <- data.frame(Timestamp, wattsupcommerce[2])
+  #Mail 
+  wattsupmail<-read.csv("wr_mail_load.TXT", sep="", header=T, row.names=NULL, stringsAsFactors=FALSE)
+  Timestamp<-as.POSIXct(as.numeric(wattsupmail[,1])*24*3600, origin="1899-12-30 00:00:00.000", tz="UTC")
+  wattsupmail <- data.frame(wattsupmail[2])
+  wattsupmail["Testtype"]<- NA
+  wattsupmail$Testtype[1:119]<-"Mail" 
   
-  #a bit messy: we have to specify that the files have the same year
-  a <- as.POSIXlt(Timestamp)$year[1]
-  b <- perfData$Timestamp
-  b$year <- a
-  perfData$Timestamp <- b
+  #Olio 
+  wattsupolio<-read.csv("wr_olio_load.TXT", sep="", header=T, row.names=NULL, stringsAsFactors=FALSE)
+  Timestamp<-as.POSIXct(as.numeric(wattsupolio[,1])*24*3600, origin="1899-12-30 00:00:00.000", tz="UTC")
+  wattsupolio <- data.frame(wattsupolio[2])
+  wattsupolio["Testtype"]<- NA
+  wattsupolio$Testtype[1:106]<-"Olio"
   
-  #again, a bit messy: we have to convert to strings and reconvert into POSIXlt to enable merging
-  wattsupData$Timestamp <- strftime(wattsupData$Timestamp, "%Y-%m-%d %H:%M:%S", tz="UTC")
-  wattsupData$Timestamp <- strptime(wattsupData$Timestamp, "%Y-%m-%d %H:%M:%S", tz="UTC")
+  write.csv(alldata, "wattsup_alldata.csv")
   
-  #finally, we can merge the data
-  practiceData<-merge(wattsupData, perfData, all=FALSE, sort=FALSE)
+  alldata<- rbind(wattsupolio,wattsupmail,wattsupcommerce)
+  testVa <- aov(alldata$Watts ~ alldata$Testtype, data = alldata)
+  tuck<- TukeyHSD(testVa)
   
-  #and return it
-  return(practiceData)
 }
